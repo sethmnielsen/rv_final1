3a95fd0902a9987a1296f5090290748776a19fcb
Seth Nielsen <sethmnielsen@gmail.com>
2019-04-12 11:02:02 -0600

    Updated mobilenet_detection

--- mobilenet_detection.py
+++ mobilenet_detection.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
-
-
-
-
-
-
-#### THANE/JUSTIN: THIS IS THE FILE THAT WILL EVOLVE INTO THE PERSON-FOLLOW NODE #####
-
-
-
-
-
-
 # # This script detects tennis balls in a video feed.
 # Be sure to follow the [installation instructions]
 # (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md)
 # before you start.
+from utils import visualization_utils as vis_util
+from utils import label_map_util
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -35,8 +24,6 @@ sys.path.append("..")
 
 # ## Object detection imports
 # Here are the imports from the object detection module.
-from utils import label_map_util
-from utils import visualization_utils as vis_util
 
 # # Model preparation
 # ## Variables
@@ -54,15 +41,16 @@ NUM_CLASSES = 90
 # ## Load a (frozen) Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
 
 # ## Loading label map
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 # # Detection
@@ -72,59 +60,72 @@ category_index = label_map_util.create_category_index(categories)
 cap = cv2.VideoCapture(0)
 
 with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
-    # Definite input and output Tensors for detection_graph
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-    # Each box represents a part of the image where a particular object was detected.
-    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    while cap.isOpened():
-        ret, frame = cap.read()
-        image_np = frame # use this line for any video feed that isn't the ZED
-        # image_np = frame[0:480, 0:640] # for using only the left camera feed if using ZED as input
+    with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name(
+            'detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name(
+            'detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name(
+            'detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        while cap.isOpened():
+            ret, frame = cap.read()
+            image_np = frame  # use this line for any video feed that isn't the ZED
+            # image_np = frame[0:480, 0:640] # for using only the left camera feed if using ZED as input
 
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        # Actual detection.
-        (boxes, scores, classes, num) = sess.run(
-          [detection_boxes, detection_scores, detection_classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
-        # Visualization of the results of a detection.
-        # vis_util.visualize_boxes_and_labels_on_image_array(
-        #   image_np,
-        #   np.squeeze(boxes),
-        #   np.squeeze(classes).astype(np.int32),
-        #   np.squeeze(scores),
-        #   category_index,
-        #   use_normalized_coordinates=True,
-        #   line_thickness=8)
-        box = boxes[0][0]
-        im_h, im_w, _ = image_np.shape
-        corners = (int(box[1]*im_w), int(box[0]*im_h), int(box[3]*im_w), int(box[2]*im_h))
-        if scores[0][0] > .50:
-            pass
-            # Instead of fancy probability box, show simple rectangle
-            # cv2.rectangle(image_np, (corners[0], corners[1]), (corners[2], corners[3]), (255,0,0), 2)
-        else:
-            corners = (-1, -1, -1, -1)
-        print(str(corners) + ', {:1.2f}'.format(scores[0][0]))
-        # cv2.imshow('object detection 3', cv2.resize(image_np, (640, 480)))
-        k = 0
-        for i in range(classes.size):
-            if classes[0][i] == 1:
-                k = i
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            # Actual detection.
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores,
+                    detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
 
-        
-            
-        cv2.imshow('object detection 2', image_np)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            cap.release()
-            break
+            sz = 5
+            boxes = np.squeeze(boxes)[:sz]
+            classes = np.squeeze(classes).astype(np.int32)[:sz]
+            scores = np.squeeze(scores)[:sz]
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+              image_np,
+              boxes,
+              classes.astype(np.int32),
+              scores,
+              category_index,
+              use_normalized_coordinates=True,
+              line_thickness=8)
+            k = -1
+            for i in range(classes.size):
+                if classes[i] == 1:
+                    k = i
+                    break
 
-    cv2.destroyAllWindows()
-    cap.release()
+            if k == -1:
+                print("No person detected")
+
+            box = boxes[k]
+            im_h, im_w, _ = image_np.shape
+            corners = (int(box[1]*im_w), int(box[0]*im_h),
+                       int(box[3]*im_w), int(box[2]*im_h))
+            if scores[k] > .50:
+                pass
+                # Instead of fancy probability box, show simple rectangle
+                # cv2.rectangle(image_np, (corners[0], corners[1]), (corners[2], corners[3]), (255,0,0), 2)
+            else:
+                corners = (-1, -1, -1, -1)
+            print('Person detected with probability: {:1f}'.format(scores[k]*100))
+            # cv2.imshow('object detection 3', cv2.resize(image_np, (640, 480)))
+
+            cv2.imshow('object detection 2', image_np)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                cap.release()
+                break
+
+        cv2.destroyAllWindows()
+        cap.release()
